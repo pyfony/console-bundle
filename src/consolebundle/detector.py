c873bde05e8731f24c7b10f0aab7556783b91850
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-06 06:38:02 +0100

    Console detector fixed

--- src/consolebundle/detector.py
+++ src/consolebundle/detector.py
@@ -1,5 +1,15 @@
 import sys
-from consolebundle.CommandRunner import runCommand
+import shutil
+from pathlib import Path
+from consolebundle import CommandRunner
 
 def isRunningInConsole():
-    return runCommand.__module__ in sys.modules
+    def compareExecutables(p1: Path, p2: Path):
+        return p1.parents[0] == p2.parents[0] and p1.stem == p2.stem
+
+    invokedUsingCommandRunner = Path(sys.argv[0]) == Path(CommandRunner.__file__)
+
+    consolePath = shutil.which('console')
+    invokedUsingScript = consolePath is not None and compareExecutables(Path(sys.argv[0]), Path(consolePath))
+
+    return invokedUsingCommandRunner or invokedUsingScript
