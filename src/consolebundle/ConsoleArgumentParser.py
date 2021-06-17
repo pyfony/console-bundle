057e55ba858951d24cc2a773bc08bf32f291d03f
jan-machek-ds <jan.machek@datasentics.com>
2021-06-17 13:57:59 +0200

    Improve CLI
    Commands now write out valid help and can also be called as space-separated subcommands in addition to the default semicolon-separated style.

--- src/consolebundle/ConsoleArgumentParser.py
+++ src/consolebundle/ConsoleArgumentParser.py
@@ -4,24 +4,33 @@ from argparse import ArgumentParser
 class ConsoleArgumentParser(ArgumentParser):
 
     __command_name = None
-    __command_name_message1 = "usage: console [-h] [-e ENV] command_name"
-    __command_name_message2 = "console: error: the following arguments are required: command_name"
+    __command_name_usage_message = "usage: console [-h] [-e ENV] command_name"
+    __command_name_error_messages = (
+        "console: error: the following arguments are required: command_name",
+        "CommandRunner.py: error: the following arguments are required: command_name"
+    )
 
     def set_command_name(self, command_name: str):
         self.__command_name = command_name
 
     def exit(self, status=0, message=None):
-        stripped_message = message.strip()
+        if message is not None:
+            stripped_message = message.strip()
 
-        if stripped_message == self.__command_name_message2:
-            return
+            if stripped_message in self.__command_name_error_messages:
+                return
 
         super().exit(status, message)
 
     def _print_message(self, message, file=None):
         stripped_message = message.strip()
 
-        if stripped_message in (self.__command_name_message1, self.__command_name_message1):
+        if stripped_message == self.__command_name_usage_message:
             return
 
-        super()._print_message(message.replace("command_name", self.__command_name), file)
+        if self.__command_name:
+            replaced_message = message.replace("command_name", self.__command_name)
+        else:
+            replaced_message = message
+
+        super()._print_message(replaced_message, file)
