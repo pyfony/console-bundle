ea42f3faea667b3843696b0a94c265c7c251a396
jan-machek-ds <jan.machek@datasentics.com>
2021-06-18 16:54:27 +0200

    Fix CLI bugs
    Logging moved to CommandRunner.
    Top-most level help corrected.
    Exit codes fixed.
    Command names definitions in command specific py files can now be both semicolon- and space-delimited.

--- src/consolebundle/CommandManager.py
+++ src/consolebundle/CommandManager.py
@@ -1,45 +1,45 @@
-from typing import List, Union
+from typing import List, Union, Tuple
 from consolebundle.ConsoleCommand import ConsoleCommand
 
 
 class CommandManager:
+
+    POSSIBLE_SEPARATORS = [':', ' ']
+
     def __init__(self, commands: List[ConsoleCommand]):
         self.__commands = commands
+
+    def get_possible_names(self, name_parts: list) -> list:
+        return [s.join(name_parts) for s in self.POSSIBLE_SEPARATORS]
+
+    def prefixes_same_on_separators(self, command_name: str, name_parts: list) -> Tuple[bool, Union[str, None]]:
+        for s in self.POSSIBLE_SEPARATORS:
+            if command_name.startswith(s.join(name_parts)):
+                return True, s
+        return False, None
 
     def get_commands(self):
         return self.__commands
 
-    def log_commands(self, logger):
-        print("\n[Available commands]:", flush=True)
-        for existing_command in self.get_commands():
-            logger.info(existing_command.get_command() + " - " + existing_command.get_description())
-
-    def log_subcommands(self, command_name: list, logger):
-        print("\n[Available space-separated commands]:", flush=True)
-        command_prefix = ':'.join(command_name)
-        for existing_command in self.get_commands():
-            if existing_command.get_command().startswith(command_prefix):
-                logger.info(
-                    f"{' '.join(existing_command.get_command().split(':'))} - " + existing_command.get_description())
-
-    def get_by_name(self, name: list) -> ConsoleCommand:
-        desired_name = ':'.join(name)
+    def get_by_name(self, name_parts: list) -> ConsoleCommand:
+        possible_names = self.get_possible_names(name_parts)
         for command in self.__commands:
-            if command.get_command() == desired_name:
+            if command.get_command() in possible_names:
                 return command
 
-        raise Exception('No command with name "{}" found'.format(name))
+        raise Exception('No command with names in "{}" found'.format(str(possible_names)))
 
-    def command_prefix_only(self, name: list) -> bool:
-        desired_name = ':'.join(name)
+    def command_prefix_only(self, name_parts: list) -> bool:
         for command in self.__commands:
-            if not command.get_command().startswith(desired_name):
+            command_name = command.get_command()
+            prefixes_same, sep = self.prefixes_same_on_separators(command_name, name_parts)
+            if not prefixes_same:
                 continue
-            split_command = command.get_command().split(':')
-            if len(name) < len(split_command):
+            split_command = command_name.split(sep)
+            if len(name_parts) < len(split_command):
                 is_sublist = True
-                for i in range(len(name)):
-                    if name[i] != split_command[i]:
+                for i in range(len(name_parts)):
+                    if name_parts[i] != split_command[i]:
                         is_sublist = False
                         break
                 if is_sublist:
